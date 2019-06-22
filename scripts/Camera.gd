b4efbc8aa266a9fa2ef29ba1ce0c739dd7f80d00
_Map_ <blendfan@gmail.com>
2019-06-22 22:42:40 +0200

    Add ground

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -4,20 +4,30 @@ extends Camera2D
 # var a = 2
 # var b = "text"
 
+export(NodePath) var player_target: NodePath
+export(NodePath) var background_target: NodePath
+
+var player: Node2D = null
+var background: Sprite = null
 var bg_frame = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if not player_target.is_empty():
+		player = get_node(player_target)
+	if not background_target.is_empty():
+		background = get_node(background_target)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if player:
+		var p_pos = player.transform.origin
+		transform.origin.x = p_pos.x
+		if background:
+			background.transform.origin.x = transform.origin.x - (transform.origin.x / 10)
+		if p_pos.y > transform.origin.y + 16:
+			transform.origin.y = p_pos.y - 16
+		elif p_pos.y < transform.origin.y - 256:
+			transform.origin.y = p_pos.y + 256
 
-
-func _on_bg_animation_timeout():
-	if bg_frame >= 25:
-		bg_frame = 1
-	else:
-		bg_frame += 1
-	$background.frame = bg_frame
